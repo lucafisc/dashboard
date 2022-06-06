d0ad45c14e4d8f2fbea8f7b2a9dfce6c76f48c56
lucafisc <lucafischer@fastmail.com>
2022-06-06 13:07:09 +0200

    Add dark mode

--- script.js
+++ script.js
@@ -8,3 +8,21 @@ searchBar.addEventListener("focus", () => {
 searchBar.addEventListener("focusout", () => {
     searchIcon.classList.remove("active");
 })
+
+//theme toggle
+const root = document.documentElement;
+const moonBtn = document.getElementById("moon-btn")
+const sunBtn = document.getElementById("sun-btn")
+moonBtn.addEventListener("click", () => {
+    toggleTheme(sunBtn, moonBtn);
+})
+sunBtn.addEventListener("click", () => {
+    toggleTheme(moonBtn, sunBtn);
+})
+
+function toggleTheme(btnShow, btnHide) {
+    const newTheme = root.className === 'dark' ? 'light' : 'dark';
+    root.className = newTheme;
+    btnHide.style.display = "none"
+    btnShow.style.display = "block"
+}
